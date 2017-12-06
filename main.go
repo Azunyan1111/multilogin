541ea5067cc99470529cc6c69052763df23328cc
Azunyan1111 <s15a1302@bene.fit.ac.jp>
2017-12-06 16:47:57 +0900

    add custom log and add header

--- main.go
+++ main.go
@@ -11,15 +11,35 @@ func main() {
 	// Echoのインスタンス作る
 	e := echo.New()
 
+	logConfig := middleware.LoggerConfig{
+		Format: `| ${time_rfc3339} | ${host}${uri} ` +
+			`| ${method} | ${status} ` +
+			`| ${latency_human}` + "\n",
+	}
+
 	// 全てのリクエストで差し込みたいミドルウェア（ログとか）はここ
-	e.Use(middleware.Logger())
+	e.Use(middleware.LoggerWithConfig(logConfig))
 	e.Use(middleware.Recover())
+	e.Use(middleware.CSRF())
+	e.Use(middleware.Gzip())
+	e.Use(customHeader)
 
 	// ルーティング
 	e.GET("/", func(e echo.Context) error {
 		return e.JSON(http.StatusOK,"Hello World")
 	})
 
-	// サーバー起動
-	e.Start(":" + os.Getenv("PORT"))    //ポート番号指定してね
+	e.Start(":" + os.Getenv("PORT"))
+}
+
+func customHeader(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		c.Response().Header().Add("X-Frame-Options", "SAMEORIGIN")
+		c.Response().Header().Add("X-XSS-Protection", "1")
+		c.Response().Header().Add("X-Content-Type-Options", "nosniff")
+		c.Response().Header().Add("Pragma", "no-cache")
+		c.Response().Header().Add("Cache-Control", "no-cache, no-store, must-revalidate")
+		err := next(c)
+		return err
+	}
 }
