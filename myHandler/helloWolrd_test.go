35477cedfbe4110e571819820375884efcc04d79
Azunyan1111 <s15a1302@bene.fit.ac.jp>
2017-12-08 22:13:04 +0900

    fix test. add testTemplate Function

--- myHandler/helloWolrd_test.go
+++ myHandler/helloWolrd_test.go
@@ -19,19 +19,25 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 }
 
 func TestHandler_HelloWorld(t *testing.T) {
-	e := echo.New()
-
-	temp := &Template{
-		templates: template.Must(template.ParseGlob("../static/views/index.html")),
-	}
-	e.Renderer = temp
-
-	req := httptest.NewRequest(echo.GET, "/", nil)
-	rec := httptest.NewRecorder()
+	e, req, rec := testTemplate("/")
 	c := e.NewContext(req, rec)
 
 	if assert.NoError(t, HelloWorld(c)) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, "Hello World", rec.Body.String())
 	}
+}
+
+func testTemplate(target string)(e *echo.Echo, req *http.Request, rec *httptest.ResponseRecorder){
+	e = echo.New()
+
+	temp := &Template{
+		templates: template.Must(template.ParseGlob("../static/views/*.html")),
+	}
+	e.Renderer = temp
+
+	req = httptest.NewRequest(echo.GET, target, nil)
+	rec = httptest.NewRecorder()
+
+	return e,req,rec
 }
