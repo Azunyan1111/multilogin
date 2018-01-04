480b080dac18c943f0f9dc42fdca8c4a8a88d32a
Azunyan1111 <s15a1302@bene.fit.ac.jp>
2018-01-05 08:03:38 +0900

    common processing to template

--- api/api.go
+++ api/api.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"github.com/labstack/echo"
 	"strconv"
+	"github.com/Azunyan1111/multilogin/structs"
+	"github.com/Azunyan1111/multilogin/mysql"
 )
 
 // http://localhost:8040/api/sum?value1=1&value2=1
@@ -33,6 +35,47 @@ func Sum(c echo.Context) error {
 	return c.JSON(http.StatusOK, res)
 }
 
+func apiTemplate(c echo.Context)(structs.Service, structs.User, JsonResponse){
+	orm := mysql.GetOrm()
+	var service structs.Service
+	var user structs.User
+	var jsonResponse JsonResponse
+
+	// パラメーターチェック
+	userUid := c.QueryParam("uuid")
+	if userUid == ""{
+		jsonResponse.StatusCode = http.StatusBadRequest
+		jsonResponse.Message = "Error: Request uuid not found. example url '/api/name?uuid=26d2983e-3d5a-421c-bf6f-d4608025e555'"
+		return service, user, jsonResponse
+	}
+	// リクエストの署名を確認する
+	if IsBadSignature(c.Request().Header.Get("Authorization")){
+		jsonResponse.StatusCode = http.StatusBadRequest
+		jsonResponse.Message = "Error: Request signature is bad."
+		return service, user, jsonResponse
+	}
+	// リクエストからサービス情報を取得する
+	if orm.Find(&service,"uuid = ?",GetServiceUid(c.Request().Header.Get("Authorization"))).RowsAffected != 1{
+		jsonResponse.StatusCode = http.StatusBadRequest
+		jsonResponse.Message = "Error: Your service is not registered. " +
+			"Also, there is no service ID in the 'Authorization' header of the request"
+		return service, user, jsonResponse
+	}
+	// ユーザーの情報を取得する
+	if orm.Find(&user,"uuid = ?",userUid).RowsAffected != 1{
+		jsonResponse.StatusCode = http.StatusBadRequest
+		jsonResponse.Message = "Error: The specified user does not exist"
+		return service, user, jsonResponse
+	}
+	// ユーザーが認証しているかを確認する
+	var con structs.ConfirmedService
+	if orm.Find(&con,"user_uuid = ? and service_uuid = ?",userUid,service.Uid).RowsAffected != 1{
+		jsonResponse.StatusCode = http.StatusBadRequest
+		jsonResponse.Message = "Error: This user is not working with your service."
+		return service, user, jsonResponse
+	}
+	return service, user, jsonResponse
+}
 
 // TODO:認証を作る。ヘッダーの中身全部打ち込まれるからスプリットしよう
 func IsBadSignature(authorization string)(bool){
--- api/getName.go
+++ api/getName.go
@@ -1,9 +1,7 @@
 package api
 
 import (
-	"github.com/Azunyan1111/multilogin/structs"
 	"github.com/labstack/echo"
-	"github.com/Azunyan1111/multilogin/mysql"
 	"net/http"
 )
 
@@ -11,47 +9,20 @@ import (
 func GetName(c echo.Context) error {
 	// response struct
 	var res NameResponse
-	// orm
-	orm := mysql.GetOrm()
 
-	userUid := c.QueryParam("uuid")
-	if userUid == ""{
-		res.JsonResponse.StatusCode = http.StatusBadRequest
-		res.JsonResponse.Message = "Error: Request uuid not found. example url '/api/name?uuid=26d2983e-3d5a-421c-bf6f-d4608025e555'"
-		return c.JSON(http.StatusBadRequest,res)
+	service, user, response := apiTemplate(c)
+	if response.StatusCode != 0{
+		c.JSON(response.StatusCode,NameResponse{JsonResponse:response})
 	}
-	if IsBadSignature(c.Request().Header.Get("Authorization")){
-		res.JsonResponse.StatusCode = http.StatusBadRequest
-		res.JsonResponse.Message = "Error: Request Signature is bad."
-		return c.JSON(http.StatusBadRequest,res)
-	}
+
 	// 権限を確認する
-	var service structs.Service
-	serviceUid := GetServiceUid(c.Request().Header.Get("Authorization"))
-	if orm.Find(&service,"uuid = ?",serviceUid).RowsAffected != 1{
-		res.JsonResponse.StatusCode = http.StatusBadRequest
-		res.JsonResponse.Message = "Error: Your service is not registered."
-		return c.JSON(http.StatusBadRequest,res)
-	}
 	if service.UserName != 1{
 		res.JsonResponse.StatusCode = http.StatusBadRequest
 		res.JsonResponse.Message = "Error: Your service does not have GetName authority."
 		return c.JSON(http.StatusBadRequest,res)
 	}
-	// ユーザーが認証しているかを確認する
-	var con structs.ConfirmedService
-	if orm.Find(&con,"user_uuid = ? and service_uuid = ?",userUid,serviceUid).RowsAffected != 1{
-		res.JsonResponse.StatusCode = http.StatusBadRequest
-		res.JsonResponse.Message = "Error: This user is not working with your service."
-		return c.JSON(http.StatusBadRequest,res)
-	}
+
 	// 権限があるのでユーザーの情報を返す
-	var user structs.User
-	if orm.Find(&user,"uuid = ?",userUid).RowsAffected != 1{
-		res.JsonResponse.StatusCode = http.StatusBadRequest
-		res.JsonResponse.Message = "Error: The specified user does not exist"
-		return c.JSON(http.StatusBadRequest,res)
-	}
 	res.JsonResponse.StatusCode = http.StatusOK
 	res.JsonResponse.Message = "ok"
 	res.Name = user.UserName
